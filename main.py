1896d3b6c513d6697f790602e6acb2bc3edcf315
imsoumya18 <soumyadeep184@gmail.com>
2021-05-17 07:40:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import pywhatkit
 
 number = input('Enter the WhatsApp no(add country code & no space): ')
 msg = input('Enter the message: ')
-hr, min = input('Enter time in format <hh:mm>: ').split(':')
+hour, minute = input('Enter time in format <hh:mm>: ').split(':')
 
-pywhatkit.sendwhatmsg(number, msg, int(hr), int(min))
+pywhatkit.sendwhatmsg(number, msg, int(hour), int(minute))
